dbebbd84223c978188510107e63894c167eb92bc
AngelZapata582 <angelzapata582@gmail.com>
2022-04-21 01:50:15 -0500

    agregacion de metodos para enviar correo; agregacion de metodo para generar codigo de autenticacion

--- app/Controllers/Http/AuthController.js
+++ app/Controllers/Http/AuthController.js
@@ -1,18 +1,69 @@
 'use strict'
-const User = use ('App/Models/User')
+const User = use('App/Models/User')
+const Mail = use('Mail')
+const Hash = use('Hash')
+const DB = use('Database')
+const Code = use('App/Models/Code')
 class AuthController {
-    async login ({request, response, auth}){
+    async login({ request, response, auth }) {
         //Campos requeridos                     //Atrapar los datos
-        const {email, password} = request.only(['email','password'])
+        const { email, password } = request.only(['email', 'password'])
         //Generar token de usuario
-        const usert = await User.findBy('email', email)
-        const token = await auth.generate(usert)
         const user = await User.findBy('email', email)
-        return response.json({
-            token: token.token,
-            username:user.Nombre
-        })
+        if (!user || await Hash.verify(password, user.password)) {
+            if (user.Rol == 1) {
+                const token = await auth.generate(user)
+                return response.json({ token: token.token, username: user.Nombre })
+            } else {
+                await this.sendmail(await this.genCode(user))
+                return response.json({ data:user.id})
+            }
+        }
+
     }
+
+    async getRol({ response, request, params }) {
+        const email = request.only('email')
+        const u = await User.find(params.id)
+        return u.Rol
+        return response.json({ nivel: rol })
+    }
+
+    async sendmail(code) {
+        const data = {
+            from: "Mailgun Sandbox <postmaster@sandboxdd93a8b366134306a7dffba91eed53d3.mailgun.org>",
+            to: "angelzapata582@gmail.com",
+            subject: "Codigo de Verificacion",
+            text: "Testing some Mailgun awesomness!"
+        };
+        try {
+            const c = {code:code}
+            await Mail.send('emails.welcome', c, (message) => {
+                message.to(data.to)
+                    .from(data.from)
+                    .subject(data.subject)
+
+            })
+        } catch (e) {
+            return e
+        }
+    }
+
+    async genCode(user) {
+        const code = Math.floor(100000 + Math.random() * 900000)
+        const ch = await Hash.make((code).toString())
+        try{
+            const c = new Code()
+            c.user_id = user.id
+            c.code = ch
+            if(await c.save()){
+                return code;
+            }
+        }catch(e) {
+            return e
+        }
+    }
+
 }
 
 module.exports = AuthController
