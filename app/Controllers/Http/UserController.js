ff70b463c30d883df9823070a703259aa47e5bed
AngelZapata582 <angelzapata582@gmail.com>
2022-04-21 01:51:17 -0500

    agregado de metodos para listar y obtener usuario

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,17 +1,49 @@
 'use strict'
 const User = use('App/Models/User')
+const Database = use('Database')
 class UserController {
-    async store({request, response}){
+    async store({ request, response }) {
         //Obtener datos que llegaran del cliente (Insomnia)
-       const userData = request.only(['Nombre','Rol','email','password'])
+        const userData = request.only(['Nombre', 'Rol', 'email', 'password'])
         //Crear Nuevo Usuario
         const user = await User.create(userData)
         //Retorna en caso de que el usuario sea creado satisfactoriamente
         return response.created({
-            status: true,
-            data: user
+            status: true
         })
-   }
+    }
+
+    async ListasUsuario({ response }) {
+        const Personas = await User.query().setHidden(['password', 'created_at', 'updated_at']).fetch()
+        response.status(200).json({
+            mensaje: "La lista de tus usuarios son:",
+            datos: Personas
+        })
+    }
+
+
+    async GetIp({ request, response }) {
+        const ip = request.ip()
+        return response.json({ ip:ip })
+    }
+
+    async getUser({ response, params }) {
+        const user = await User.query()
+            .setHidden(['password', 'created_at', 'updated_at'])
+            .where('id',params.id)
+            .first()
+
+        return response.json({ user:user })
+    }
+
+    async EliminarUsuario ({response, params}){
+    const users = await User.find(params)
+    await users.delete()
+    return response.json({
+        message: "Eliminado con exito"
+    })
+    }
+    
 }
 
 module.exports = UserController
