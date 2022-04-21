5193a890bb06134757c7a1f55e30d3783cc1f4bd
AngelZapata582 <angelzapata582@gmail.com>
2022-04-21 01:50:46 -0500

    agregado de funcionalidad para verificar el codigo

--- app/Controllers/Http/CodeController.js
+++ app/Controllers/Http/CodeController.js
@@ -1,9 +1,25 @@
 'use strict'
-const Codes = use ('App/Models/Codigo')
+const Codes = use ('App/Models/Code')
+const DB = use('Database')
+const Hash = use('Hash')
 class CodeController {
-    async VerificarCodigo ({}){
-        //Código de verificación de código
+    async VerificarCodigo ({request, response,params}){
         
+        //const code = await Codes.findBy('user_id',params.id)
+        const code = await Codes.query().select('code').where({user_id:params.id}).last()
+        const c = request.only('code')
+        if(code && await Hash.verify(c.code,code.code))
+        {
+            return true
+        }
+        else{
+            return false
+        }
+        
+    }
+
+    async VerificarAuth({request, response, params}){
+        //verificacion de autorizacion para accion de 3er usuario
     }
 }
 
