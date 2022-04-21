9639187835b36a44f8136a06e700dea57c556d2a
AngelZapata582 <angelzapata582@gmail.com>
2022-04-21 01:51:41 -0500

    cambio de nombre

--- app/Models/Code.js
+++ app/Models/Code.js
@@ -3,7 +3,7 @@
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
 
-class Codigo extends Model {
+class Code extends Model {
 }
 
-module.exports = Codigo
+module.exports = Code
