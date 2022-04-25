d90baff445afe1dd0ac1eb3e66f28e2251a4c6f4
AngelZapata582 <angelzapata582@gmail.com>
2022-04-24 22:16:14 -0500

    agregado de metodos de autenticacion

--- app/Controllers/Http/AuthController.js
+++ app/Controllers/Http/AuthController.js
@@ -13,10 +13,10 @@ class AuthController {
         if (!user || await Hash.verify(password, user.password)) {
             if (user.Rol == 1) {
                 const token = await auth.generate(user)
-                return response.json({ token: token.token, username: user.Nombre })
+                return response.json({status:true, token: token.token, username: user.Nombre })
             } else {
                 await this.sendmail(await this.genCode(user))
-                return response.json({ data:user.id})
+                return response.json({status:false, data:user.id})
             }
         }
 
@@ -64,6 +64,43 @@ class AuthController {
         }
     }
 
+    async createCodeVerificationSU({response, params}){
+        const c = Math.floor(100000 + Math.random() * 900000)
+        //const ch = await Hash.make((c).toString())
+        if(await User.find(params.id)){
+            await DB.table('auth_codes').insert({user_id:params.id, code:c})
+            return response.json({status:true,code:c})
+        }else{
+            return response.json({status:false})
+        }
+        
+    }
+
+    async codeVerificationSU({request, response}){
+        const user = await User.findBy('email',request.only('email').email)
+        
+        if(user && await Hash.verify(request.only('password').password,user.password)){
+            const code = await DB.table('auth_codes').select('code').where('user_id',user.id).last()
+            return response.json({status:true,code:code.code})
+        }else{
+            return false;
+        }
+    }
+
+    async codeVerification({request, response, auth}){
+        const {id,vcode} = request.only(['id', 'vcode'])
+        
+        const vcodev = await DB.table('auth_codes').select('code').where('user_id',id).last()
+        
+        const user = await User.find(id)
+        if(vcodev.code == vcode){
+            const token = await auth.generate(user)
+            return response.json({status:true,token:token.token})
+        }
+        else{
+            return response.json({status:false})
+        }
+    }
 }
 
 module.exports = AuthController
--- app/Controllers/Http/CodeController.js
+++ app/Controllers/Http/CodeController.js
@@ -2,24 +2,36 @@
 const Codes = use ('App/Models/Code')
 const DB = use('Database')
 const Hash = use('Hash')
+const User = use('App/Models/User')
 class CodeController {
-    async VerificarCodigo ({request, response,params}){
+    async VerificarCodigo ({request, response,params,auth}){
         
-        //const code = await Codes.findBy('user_id',params.id)
+        const user = await User.find(params.id)
         const code = await Codes.query().select('code').where({user_id:params.id}).last()
         const c = request.only('code')
         if(code && await Hash.verify(c.code,code.code))
         {
-            return true
+            if(user && user.Rol == 3){
+                return response.json({status:true,loged:false,sala:await this.genCode()})
+            }else{
+                const token = await auth.generate(user)
+                await Codes.query().where('user_id',params.id).delete()
+                return response.json({ loged:true,token: token.token, 
+                    username: user.Nombre,status:true})
+            }
         }
         else{
-            return false
+            return response.json({status:false})
         }
         
     }
 
     async VerificarAuth({request, response, params}){
         //verificacion de autorizacion para accion de 3er usuario
+    }
+
+    async genCode() {
+        return Math.floor(100000 + Math.random() * 900000)
     }
 }
 
